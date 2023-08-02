b2e248d3e3ac91255f5beb102ffce144ab019a5b
Roshan <shahroshan0720@gmail.com>
2023-08-02 21:39:27 +0530

    middleware added

--- app.js
+++ app.js
@@ -4,12 +4,17 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+const notFound = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+
 app.use(express.json());
 
 app.get('/',(req,res)=>{
     res.send('Stripe Payment');
 })
 
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 const port = process.env.PORT || 5000;
 
 app.listen(port,()=>{
